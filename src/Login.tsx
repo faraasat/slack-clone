a261e109353f8b08415ff7b930bebd08a78e47a2
Farasat Ali <farasataliazeemi1@gmail.com>
2021-04-15 22:00:14 +0500

    Updated the App

---
+++ src/Login.tsx
@@ -0,0 +1,33 @@
+import "./Login.css";
+import { Button } from "@material-ui/core";
+import { auth, provider } from "./firebase";
+
+const Login = () => {
+  const signIn = (e: any) => {
+    e.preventDefault();
+    auth
+      .signInWithPopup(provider)
+      .then((result: any) => {
+        console.log(result);
+      })
+      .catch((error: any) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="login">
+      <div className="login__container">
+        <img
+          src="https://cdn.worldvectorlogo.com/logos/slack-new-logo.svg"
+          alt="Slack"
+        />
+        <h1>Sign In to Clever Programmer HQ</h1>
+        <p>cleverprogrammer.slack.com</p>
+        <Button onClick={signIn}>Sign In with Google</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
